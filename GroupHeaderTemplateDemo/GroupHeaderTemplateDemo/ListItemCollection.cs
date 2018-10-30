d0d826774284f0deee50828d68b1ac67977e107c
Land Lu <v-luting@microsoft.com>
2018-10-30 10:08:28 +0800

    Add project files.

---
+++ GroupHeaderTemplateDemo/GroupHeaderTemplateDemo/ListItemCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GroupHeaderTemplateDemo
+{
+public class ListItemCollection : ObservableCollection<Item>
+{
+    public string Title { set; get; }
+
+    public ListItemCollection(string title)
+    {
+        Title = title;
+
+        ExpandCommand = new Command<ListItemCollection>((listItem) =>
+        {
+
+        });
+    }
+
+    public Command<ListItemCollection> ExpandCommand { set; get; }
+}
+
+    public class Item
+    {
+        public string Name { set; get; }
+        public string Description { set; get; }
+    }
+}
---
+++ GroupHeaderTemplateDemo/GroupHeaderTemplateDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GroupHeaderTemplateDemo
+{
+	public partial class MainPage : ContentPage
+	{
+        ObservableCollection<ListItemCollection> list;
+        public MainPage()
+		{
+			InitializeComponent();
+
+            list = new ObservableCollection<ListItemCollection>();
+            for (int i=0; i<10; i++)
+            {
+                ListItemCollection items = new ListItemCollection("title" + i);
+                for (int j=0; j<10; j++)
+                {
+                    items.Add(new Item { Name = "name" + j, Description = "Description" + j });
+                }
+                list.Add(items);
+            }
+            itemListView.ItemsSource = list;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            itemListView.ScrollTo(list[2][3], list[2], ScrollToPosition.Start, true);
+        }
+    }
+}
